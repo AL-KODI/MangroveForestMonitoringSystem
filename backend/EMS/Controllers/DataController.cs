42d1fdfeb4e58041f0ab1cb16a44a6f49fbd658a
AL-KODI <lakmalarunascc@gmail.com>
2026-02-07 14:01:42 +0530

    Initial Commit

---
+++ backend/EMS/Controllers/DataController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Controllers
+{
+    using EMS.Hubs;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.SignalR;
+
+    [ApiController]
+   
+    [Route("Data")]
+    public class DataController : ControllerBase
+    {
+        private readonly IHubContext<DataHub> _hub;
+
+        public DataController(IHubContext<DataHub> hub)
+        {
+            _hub = hub;
+        }
+
+        [Authorize]
+        [HttpGet("{value}")]
+        public async Task<IActionResult> Send(int value)
+        {
+            await _hub.Clients.All.SendAsync("ReceiveData", value);
+            return Ok();
+
+        }
+       
+
+
+    }
+}
---
+++ backend/EMS/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace EMS.Data
+{
+
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
+
